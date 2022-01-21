b5566b3131d958dba3e30f05841f727c55f22a8e
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-21 19:22:08 +0800

    styling desktop-design

---
+++ script.js
@@ -0,0 +1,13 @@
+// const hover = document.querySelectorAll('.detail');
+
+// hover.forEach(function(card) {
+//     card.addEventListener('mouseover', function() {
+//         card.classList.add('detail-hover');
+//     })
+//     if (card.classList.contains('detail-hover')) {
+//         card.classList.remove('detail-hover');
+//     }
+// });
+// hover.forEach(function(card) {
+//     card.classList.remove('detail-hover');
+// })
