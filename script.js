bf51bbfaeb99c055e4f3f275c993177e1523ff6e
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-22 18:00:09 +0800

    add readme amd some styling animation

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// const hover = document.querySelectorAll('.detail');
-
-// hover.forEach(function(card) {
-//     card.addEventListener('mouseover', function() {
-//         card.classList.add('detail-hover');
-//     })
-//     if (card.classList.contains('detail-hover')) {
-//         card.classList.remove('detail-hover');
-//     }
-// });
-// hover.forEach(function(card) {
-//     card.classList.remove('detail-hover');
-// })
-
-
 var myInit = {
     method: 'GET',
     headers: {
@@ -24,12 +9,33 @@ var myInit = {
 
 let myRequest = new Request("./data.json", myInit);
 
-// fetch(myRequest)
-//     .then(response => response.json())
-//     .then(data => {
-//         myData = data;
-//         return myData
-//     });
+function showData(cards, data, time) {
+    for (const i in data) {
+        const timeFrame = time.textContent.toLowerCase();
+        const current = cards[i].lastElementChild.lastElementChild.firstElementChild.firstElementChild;
+        const previous = cards[i].lastElementChild.lastElementChild.lastElementChild.lastElementChild;
+        const previousTime = cards[i].lastElementChild.lastElementChild.lastElementChild.firstElementChild;
+        const frameTime = [...timeFrame];
+        frameTime.splice(-2, 2);
+        if (frameTime[frameTime.length - 1] == 'i') {
+            frameTime[frameTime.length - 1] = 'y';
+        }
+
+        addFade([current, previous])
+        current.innerHTML = data[i].timeframes[timeFrame].current;
+        previousTime.innerHTML = frameTime.join('');
+        previous.innerHTML = data[i].timeframes[timeFrame].previous;
+    }
+}
+
+function addFade(param) {
+    param.forEach(function(el) {
+        el.parentElement.classList.add('change');
+        setTimeout(function() {
+            el.parentElement.classList.remove('change')
+        }, 500)
+    })
+}
 
 const times = document.querySelectorAll('.time');
 times.forEach(function(time) {
@@ -38,28 +44,7 @@ times.forEach(function(time) {
             .then(response => response.json())
             .then(data => {
                 const cards = document.querySelectorAll('.card');
-                for (const i in data) {
-                    const timeFrame = time.textContent.toLowerCase();
-                    const current = cards[i].lastElementChild.lastElementChild.firstElementChild.firstElementChild;
-                    const previous = cards[i].lastElementChild.lastElementChild.lastElementChild.lastElementChild;
-                    const previousTime = cards[i].lastElementChild.lastElementChild.lastElementChild.firstElementChild;
-                    const frameTime = [...timeFrame];
-                    frameTime.splice(-2, 2);
-                    if (frameTime[frameTime.length - 1] == 'i') {
-                        frameTime[frameTime.length - 1] = 'y';
-                    }
-
-
-                    current.innerHTML = data[i].timeframes[timeFrame].current;
-                    previousTime.innerHTML = frameTime.join('');
-                    previous.innerHTML = data[i].timeframes[timeFrame].previous;
-                }
+                showData(cards, data, time);
             });
     })
 })
-
-// function addData (timeframe, data) {
-//     timeframe.forEach(function(time) {
-//         if(timeframe.lastChildElement.firstChildElement.textContent == data.)
-//     })
-// }
